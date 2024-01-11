eea63912bd547603861781e4a445348c2c172b7e
serio2010 <serio2010@gmail.com>
2024-01-11 19:42:41 +0900

    F_LOCK
    F_LOCK-TEST3

--- frequencies.c
+++ frequencies.c
@@ -171,7 +171,7 @@ int32_t TX_freq_check(const uint32_t Frequency)
 
 	switch (gSetting_F_LOCK)
 	{
-/*
+
 			case F_LOCK_DEF:
 			if (Frequency >= frequencyBandTable[BAND3_137MHz].lower && Frequency < frequencyBandTable[BAND3_137MHz].upper)
 				return 0;
@@ -222,10 +222,10 @@ int32_t TX_freq_check(const uint32_t Frequency)
 			if (Frequency >= 40000000 && Frequency < 43800000)
 				return 0;
 			break;
-*/
+
 		case F_LOCK_ALL:
 			break;
-/*
+
 		case F_LOCK_NONE:
 			for (uint32_t i = 0; i < ARRAY_SIZE(frequencyBandTable); i++)
 				if (Frequency >= frequencyBandTable[i].lower && Frequency < frequencyBandTable[i].upper)
